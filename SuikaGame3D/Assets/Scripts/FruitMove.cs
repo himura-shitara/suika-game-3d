fa3792166cfa0d0e44dc010daff46080fb087532
himura-shitara <113117363+himura-shitara@users.noreply.github.com>
2024-04-26 03:29:26 +0900

    Level5: フルーツを落とした後に補充されるようにしよう (#7)

--- SuikaGame3D/Assets/Scripts/FruitMove.cs
+++ SuikaGame3D/Assets/Scripts/FruitMove.cs
@@ -1,14 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class FruitMove : MonoBehaviour
 {
-    [SerializeField] private GameObject fruit;
+    [SerializeField] private GameObject[] fruits;
+    [SerializeField] private Transform initialPoint;
+    private GameObject _fruit;
     private readonly float _speed = 3f;
     private Rigidbody _fruitRb;
 
     private void Start()
     {
-        _fruitRb = fruit.GetComponent<Rigidbody>();
+        InstantiateRandomFruit(limitIndex: fruits.Length - 3);
     }
 
     private void Update()
@@ -16,16 +19,29 @@ public class FruitMove : MonoBehaviour
         var inputX = Input.GetAxis("Horizontal");
         var inputY = Input.GetAxis("Vertical");
 
-        if (fruit is not null)
+        if (_fruit is not null)
         {
-            fruit.transform.position += new Vector3(inputX, 0, inputY) * (_speed * Time.deltaTime);
+            _fruit.transform.position += new Vector3(inputX, 0, inputY) * (_speed * Time.deltaTime);
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _fruitRb.useGravity = true;
-                fruit = null;
+                _fruit = null;
                 _fruitRb = null;
+                StartCoroutine(DelayMethod(delay: 1f));
             }
         }
+    }
+    
+    private IEnumerator DelayMethod(float delay) {
+        yield return new WaitForSeconds(delay);
+        InstantiateRandomFruit(limitIndex: fruits.Length - 2);
+    }
+
+    private void InstantiateRandomFruit(int limitIndex)
+    {
+        var index = Random.Range(0, limitIndex);
+        _fruit = Instantiate(fruits[index], initialPoint.position, Quaternion.identity);
+        _fruitRb = _fruit.GetComponent<Rigidbody>();
     }
 }
