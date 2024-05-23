7eedddc67633bde43ef0f5ae33ebd1c26910a3b6
Akito Shitara <himura@Akitos-MacBook-Pro.local>
2024-05-23 23:56:38 +0900

    refactor: comment and reformat code

--- SuikaGame3D/Assets/Scripts/FruitMove.cs
+++ SuikaGame3D/Assets/Scripts/FruitMove.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class FruitMove : MonoBehaviour
 {
-    [SerializeField] private GameObject fruit;
-    private readonly float _speed = 3f;
+    public GameObject fruit;
+    private float _speed = 3f;
 
     private void Update()
     {
+        // Input.GetAxis("Horizontal") で左右方向の入力を取得する
         var inputX = Input.GetAxis("Horizontal");
+        // Input.GetAxis("Vertical") で上下方向の入力を取得する
         var inputY = Input.GetAxis("Vertical");
 
+        // フルーツの移動
         fruit.transform.position += new Vector3(inputX, 0, inputY) * (_speed * Time.deltaTime);
     }
 }
