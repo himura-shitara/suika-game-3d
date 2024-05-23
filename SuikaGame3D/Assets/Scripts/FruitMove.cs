a1011144ab2c6d42bd19f980fb86800f7482a153
himura-shitara <113117363+himura-shitara@users.noreply.github.com>
2024-05-24 00:49:04 +0900

    Level 5: プレイヤーの入力によってフルーツを落下させられるようにしよう (#24)

--- SuikaGame3D/Assets/Scripts/FruitMove.cs
+++ SuikaGame3D/Assets/Scripts/FruitMove.cs
@@ -4,6 +4,12 @@ public class FruitMove : MonoBehaviour
 {
     public GameObject fruit;
     private float _speed = 3f;
+    private Rigidbody _fruitRigidbody;
+
+    private void Start()
+    {
+        _fruitRigidbody = fruit.GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -12,7 +18,20 @@ public class FruitMove : MonoBehaviour
         // Input.GetAxis("Vertical") で上下方向の入力を取得する
         var inputY = Input.GetAxis("Vertical");
 
-        // フルーツの移動
-        fruit.transform.position += new Vector3(inputX, 0, inputY) * (_speed * Time.deltaTime);
+        // フルーツが null でない（フルーツへのアクセスを持っている）なら
+        if (fruit != null)
+        {
+            // フルーツの移動
+            fruit.transform.position += new Vector3(inputX, 0, inputY) * (_speed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // フルーツの重力をオン
+                _fruitRigidbody.useGravity = true;
+                // フルーツを null にする（フルーツへのアクセスを手放す）
+                fruit = null;
+                _fruitRigidbody = null;
+            }
+        }
     }
 }
